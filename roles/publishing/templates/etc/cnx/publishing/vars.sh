806e53acdaa396fd310cc3914233313a57895684
dtwilliamson <dtw@rice.edu>
2017-04-21 18:00:13 -0500

    add vars.sh files to hold shell vars for script use

---
+++ roles/publishing/templates/etc/cnx/publishing/vars.sh
@@ -0,0 +1,14 @@
+# this is a shell script which can be sourced into other scripts
+# to provide access to configuration values specific to this host
+
+publishing_host="{{ publishing_host }}"
+publishing_port="{{ publishing_port }}"
+publishing_base_port="{{ publishing_base_port }}"
+publishing_count="{{ publishing_count }}"
+# NOTE: some of the hosts in this list may point to a different db_host
+publishing_hosts_list=( "{{ groups.publishing | join(" ") }}" )
+
+archive_db_name="{{ vault_archive_db_name }}"
+archive_db_user="{{ vault_archive_db_user }}"
+archive_db_host="{{ archive_db_host }}"
+archive_db_port="{{ archive_db_port }}"
