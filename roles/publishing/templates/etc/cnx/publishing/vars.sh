a5b4b45a85033af1d6456ec6f2faba1c2faea9cd
Michael Mulich <michael.mulich@gmail.com>
2017-05-11 13:21:21 -0700

    Remove the now invalid publishing_port variable

--- roles/publishing/templates/etc/cnx/publishing/vars.sh
+++ roles/publishing/templates/etc/cnx/publishing/vars.sh
@@ -1,7 +1,6 @@
 # this is a shell script which can be sourced into other scripts
 # to provide access to configuration values specific to this host
 
-publishing_port="{{ publishing_port }}"
 publishing_base_port="{{ publishing_base_port }}"
 publishing_count="{{ publishing_count }}"
 # NOTE: some of the hosts in this list may point to a different db_host
