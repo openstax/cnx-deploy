0120d9dc4c5d604001623d706bf971610a2a1529
Michael Mulich <michael.mulich@gmail.com>
2017-05-26 15:28:20 -0700

    Add backward compatible shim for channel-processing feature

---
+++ roles/channel_processing/files/usr/local/bin/xxx_channel_processing.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+###
+# BBB (26-May-12017) mv-post-publishing-to-channel-processing
+# This script exists to bridge deployment between feature changes.
+###
+
+# TODO Code this to use one of two scripts:
+#      cnx-publishing-post-publication or cnx-publishing-channel-processing
