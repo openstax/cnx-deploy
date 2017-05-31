743d1d459885f7848484cb01f87e9914d34ad1e3
Michael Mulich <michael.mulich@gmail.com>
2017-05-31 09:38:17 -0700

    Supply default values for base_port and count

--- roles/archive/templates/etc/cnx/archive/vars.sh
+++ roles/archive/templates/etc/cnx/archive/vars.sh
@@ -5,7 +5,7 @@ archive_db_name="{{ vault_archive_db_name }}"
 archive_db_user="{{ vault_archive_db_user }}"
 archive_db_host="{{ archive_db_host }}"
 archive_db_port="{{ archive_db_port }}"
-archive_base_port="{{ default_archive_base_port }}"
-archive_count="{{ archive_count }}"
+archive_base_port="{{ archive_base_port|default(default_archive_base_port) }}"
+archive_count="{{ archive_count|default(1) }}"
 # NOTE: some of the hosts in this list may point to a different db_host
 archive_hosts_list=( "{{ groups.archive | join(" ") }}" )
