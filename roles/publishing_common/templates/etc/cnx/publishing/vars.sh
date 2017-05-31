69719386a618dbc2f090943e56b5cf005191e5e3
Michael Mulich <michael.mulich@gmail.com>
2017-05-31 09:38:17 -0700

    Supply default values for base_port and count

--- roles/publishing_common/templates/etc/cnx/publishing/vars.sh
+++ roles/publishing_common/templates/etc/cnx/publishing/vars.sh
@@ -1,8 +1,8 @@
 # this is a shell script which can be sourced into other scripts
 # to provide access to configuration values specific to this host
 
-publishing_base_port="{{ publishing_base_port }}"
-publishing_count="{{ publishing_count }}"
+publishing_base_port="{{ publishing_base_port|default(default_publishing_base_port) }}"
+publishing_count="{{ publishing_count|default(1) }}"
 # NOTE: some of the hosts in this list may point to a different db_host
 publishing_hosts_list=( "{{ groups.publishing | join(" ") }}" )
 
