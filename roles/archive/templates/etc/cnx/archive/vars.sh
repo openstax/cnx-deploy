d70791bd431dbc9a5d179235992ec97298898589
karen chan <karen@karen-chan.com>
2017-07-27 08:29:26 +0100

    Use the non-vault variable in template
    The template was using "vault_archive_db_name" and
    "vault_archive_db_user" which may not be defined in all environments.
    
    The vault version of the variables should probably only be used in the
    environment vars.yml files and in templates and playbooks, the non-vault
    version should be used.

--- roles/archive/templates/etc/cnx/archive/vars.sh
+++ roles/archive/templates/etc/cnx/archive/vars.sh
@@ -1,8 +1,8 @@
 # this is a shell script which can be sourced into other scripts
 # to provide access to configuration values specific to this host
 
-archive_db_name="{{ vault_archive_db_name }}"
-archive_db_user="{{ vault_archive_db_user }}"
+archive_db_name="{{ archive_db_name }}"
+archive_db_user="{{ archive_db_user }}"
 archive_db_host="{{ archive_db_host }}"
 archive_db_port="{{ archive_db_port }}"
 archive_base_port="{{ archive_base_port|default(default_archive_base_port) }}"
