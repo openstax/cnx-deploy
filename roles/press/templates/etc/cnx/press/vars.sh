77f6607ff20db6590fec643499748438b48fca83
Michael Mulich <michael.mulich@gmail.com>
2018-02-26 15:11:06 -0800

    Add the Press application

---
+++ roles/press/templates/etc/cnx/press/vars.sh
@@ -0,0 +1,6 @@
+# this is a shell script which can be sourced into other scripts
+# to provide access to configuration values specific to this application
+
+SHARED_DIR="/var/cnx/apps/var"
+DB_URL="postgresql://{{ press_db_user }}:{{ press_db_password }}@{{ press_db_host }}:{{ press_db_port }}/{{ press_db_name }}"
+DB_SUPER_URL="postgresql://postgres@{{ press_db_host }}:{{ press_db_port }}/{{ press_db_name }}"
