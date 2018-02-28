0cad9ac9b8cd16d38f272b3e5c57f5ce251a3ad6
Michael Mulich <michael.mulich@gmail.com>
2018-02-28 11:27:54 -0800

    Configured Sentry for Press

--- roles/press/templates/etc/cnx/press/vars.sh
+++ roles/press/templates/etc/cnx/press/vars.sh
@@ -4,3 +4,4 @@
 SHARED_DIR="/var/cnx/apps/var"
 DB_URL="postgresql://{{ press_db_user }}:{{ press_db_password }}@{{ press_db_host }}:{{ press_db_port }}/{{ press_db_name }}"
 DB_SUPER_URL="postgresql://postgres@{{ press_db_host }}:{{ press_db_port }}/{{ press_db_name }}"
+SENTRY_DSN="{{ sentry_dsn|default() }}"
